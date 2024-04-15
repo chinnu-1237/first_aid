6bfc6bf6b7d972a335ebeca6e82ed06c30ce29f2
chinnu-1237 <84765368+chinnu-1237@users.noreply.github.com>
2024-04-16 00:29:08 +0530

    Update firstaid.py

--- firstaid.py
+++ firstaid.py
@@ -158,7 +158,7 @@ with col1:
         show_treatment("Eye Injury")
 
 # Clear button to clear all selections
-if st.button("Clear All", key="Clear",  bg_color="#4b0082", text_color="#ffffff"):
+if st.button("Clear All", key="Clear"):
     st.experimental_rerun()
 
 
